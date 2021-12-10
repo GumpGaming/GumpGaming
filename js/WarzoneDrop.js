ef736102dbbcde1ee3472eb9bd82ddeb6bb8df08
GumpGaming <anxin4392@gmail.com>
2021-12-10 11:01:08 -0500

    Updating to new drop locations

--- js/WarzoneDrop.js
+++ js/WarzoneDrop.js
@@ -3,15 +3,40 @@ $.noConflict()(function($) {
     var randomNumber;
     var randomSpot;
     var randomQuote;
-
+    /*
+        function getSpot() {
+          var Spots = ["AFK", "Train", "B3", "B4", "B5", "B6", "B7", "C1", "C2", "C3",
+            "C4", "C5", "C6", "C7", "C8", "D1", "D2", "D3", "D4", "D5", "D6",
+            "D7", "D8", "E1", "E2", "E3", "E4", "E5", "E6", "E7", "E8", "F1",
+            "F2", "F3", "F4", "F5", "F6", "F7", "F8", "G2", "G3", "G4", "G5",
+            "G6", "G7", "G8", "H2", "H3", "H4", "H5", "H6", "H7", "H8", "I4",
+            "I5", "I6", "I7", "I8", "AFK", "Train"
+          ];
+    */
     function getSpot() {
-      var Spots = ["AFK", "Train", "B3", "B4", "B5", "B6", "B7", "C1", "C2", "C3",
-        "C4", "C5", "C6", "C7", "C8", "D1", "D2", "D3", "D4", "D5", "D6",
-        "D7", "D8", "E1", "E2", "E3", "E4", "E5", "E6", "E7", "E8", "F1",
-        "F2", "F3", "F4", "F5", "F6", "F7", "F8", "G2", "G3", "G4", "G5",
-        "G6", "G7", "G8", "H2", "H3", "H4", "H5", "H6", "H7", "H8", "I4",
-        "I5", "I6", "I7", "I8", "AFK", "Train"
+      var Spots = ["B3", "B4", "B5", "B6",
+        "C1", "C2", "C3", "C4", "C5", "C6", "C7",
+        "D1", "D2", "D3", "D4", "D5", "D6", "D7",
+        "E0", "E1", "E2", "E3", "E4", "E5", "E6", "E7", "E8", "E9",
+        "F0", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9",
+        "G0", "G1", "G2", "G3", "G4", "G5", "G6", "G7", "G8", "G9",
+        "H1", "H2", "H3", "H4", "H5", "H6", "H7", "H8",
+        "I1", "I2", "I3", "I4", "I5", "I6", "I7", "I8",
+        "Jump At Start Of Plane",
+        "Jump At End Of Plane",
+        "Follow an Enemy To The Ground",
+        "Follow an Enemy To The Ground",
+        "First Contract On Plane Path",
+        "Center Of Circle",
+        "Farthest Area From Circle",
+        "Top of Circle",
+        "Bottom Of Circle",
+        "Left Side Of Circle",
+        "Right Side Of Circle",
+        "Last Contract On Plane Path",
+        "Any No Named Location"
       ];
+
 
       var Quotes = ["Never in the field of human conflict was so much owed by so many to so few. — Winston Churchill",
         "Success is not final, failure is not fatal: it is the courage to continue that counts. — Winston Churchill",
